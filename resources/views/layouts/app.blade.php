9e4e14b6b9fd0ec85974f62c9dc2e0edd081a2c8
DillanINF <dilaninf6@gmail.com>
2025-10-14 16:38:26 +0700

    new update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('image/LOGO.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('image/LOGO.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('image/LOGO.png') }}">
     <script>
         (function(){
             try {
